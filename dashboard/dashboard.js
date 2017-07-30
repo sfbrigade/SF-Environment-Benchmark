defd64eaeb6bc987032cfd171b1a8875226fb051
Eric Youngson <eric@successionecological.com>
2017-07-30 15:21:51 -0700

    Added temporary (1 year) fix to issue 74

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -510,7 +510,9 @@ function populateInfoBoxes (singleBuildingData,categoryData,floorAreaRange) {
   d3.select('#building-ranking').text(euirank[0])
   d3.select('#total-building-type').text(euirank[1])
 
-  var complianceStatusIndicator = (singleBuildingData.latest_benchmark == "Complied") ?
+  // TODO (eayoungs@gmail.com): Update latest() function to work for benchmark status (benchmark_2016_status
+  //                            is a temporary fix for the current year)
+  var complianceStatusIndicator = (singleBuildingData.benchmark_2016_status == "Complied") ?
     ' <i class="fa fa-check" aria-hidden="true"></i>'
     :
     ' <i class="fa fa-times attn" aria-hidden="true"></i>'

5c1218d47b4c373950a6bef73b756f24c0be9571
thfield <thfield@gmail.com>
2017-08-21 14:42:21 -0700

    fix display of lower bound of floor area

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -161,7 +161,7 @@ function getUrlVars() {
 * @return {array} the 'where array'
 */
 function whereArray(propertyType, range){
-  if (range[0] == undefined) {range[0] = 0}
+  if (range[0] == undefined) {range[0] = 10000}
   let res = [
     "property_type_self_selected='" + propertyType + "'",
     'floor_area > ' + range[0]

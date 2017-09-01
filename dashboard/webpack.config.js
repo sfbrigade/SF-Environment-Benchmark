49c281b72dee9ba8dcad7114a363d7156054c558
thfield <thfield@gmail.com>
2017-09-01 13:55:35 -0700

    include sfenvironment logo asset

--- dashboard/webpack.config.js
+++ dashboard/webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist'
   },
